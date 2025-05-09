0cf43a9c3bd87f876b4ae80551c67acc66045e19
TuanAnhVu081 <anhvtse172464@fpt.edu.vn>
2025-05-09 18:12:47 +0700

    Add Spring Boot project

---
+++ src/main/java/exe201/Refashion/RefashionApplication.java
@@ -0,0 +1,13 @@
+package exe201.Refashion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefashionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefashionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/exe201/Refashion/RefashionApplicationTests.java
@@ -0,0 +1,13 @@
+package exe201.Refashion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefashionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
